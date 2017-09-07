e5246734a26bdc44eed281860ca6cf4ece4cf088
dmf2016 <dengmingfeng@163.com>
2017-09-07 13:49:11 +0800

    d

--- src/test/java/com/redis/TestRedisTemplate.java
+++ src/test/java/com/redis/TestRedisTemplate.java
@@ -1,5 +1,7 @@
 package com.redis;
 
+import java.util.Iterator;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
@@ -25,13 +27,64 @@ public class TestRedisTemplate {
 	@Autowired
 	private RedisTemplate<String, Object> redisTemplate;
 
+	/**
+	 * 从右边增加 TODO
+	 * @Date:Sep 7, 2017
+	 * @Author dmf
+	 */
 	@Test
-	public void addLink() {
+	public void addRightLink() {
 		// 添加 一个 list 列表
 		ListOperations<String, Object> list = redisTemplate.opsForList();
-		list.rightPush("lpList", "lp");
-		list.rightPush("lpList", "26");
+		list.rightPush("lpList", "1");
+		list.rightPush("lpList", "2");
+		for (int i = 0; i < list.size("lpList"); i++) {
+			String str = (String) list.index("lpList", i);
+			System.out.println(str);
+		}
+	}
+	/**
+	 * 从左边增加 TODO
+	 * @Date:Sep 7, 2017
+	 * @Author dmf
+	 */
+	@Test
+	public void addLeftLink() {
+		// 添加 一个 list 列表
+		ListOperations<String, Object> list = redisTemplate.opsForList();
+		list.leftPush("lpList", "a");
+		list.leftPush("lpList", "b");
 		// 输出 list
-		System.out.println(list.range("lpList", 0, 1));
+		for (int i = 0; i < list.size("lpList"); i++) {
+			String str = (String) list.index("lpList", i);
+			System.out.println(str);
+		}
+	}
+	/**
+	 * 出队列 TODO
+	 * @Date:Sep 7, 2017
+	 * @Author dmf
+	 */
+	@Test
+	public void popLeftLink() {
+		// 添加 一个 list 列表
+		ListOperations<String, Object> list = redisTemplate.opsForList();
+		// 输出 list
+		for (int i = 0; i < list.size("lpList"); i++) {
+			String str = (String) list.index("lpList", i);
+			System.out.println("-----1--:" + str);
+		}
+		Iterator it = (Iterator) list.rightPop("lpList");
+		while (it.hasNext()) {
+			String str = (String) it.next();
+			System.out.println("-----2--:" + str);
+			// 出队列
+			list.leftPop("lpList");
+		}
+		// 再显示队列数据
+		for (int i = 0; i < list.size("lpList"); i++) {
+			String str = (String) list.index("lpList", i);
+			System.out.println("-----3--:" + str);
+		}
 	}
 }
